7ce2935165bf9767eb4ad45bcebf211c761b3658
takashi-okimoto <okimaru.03@icloud.com>
2020-12-20 13:47:46 +0900

    Cloud Functions add API

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKJYclU1sWtPvC0G0EeYqhEkwYubkAzqo",
+  authDomain: "oki-sample-76bb4.firebaseapp.com",
+  projectId: "oki-sample-76bb4",
+  storageBucket: "oki-sample-76bb4.appspot.com",
+  messagingSenderId: "493675019602",
+  appId: "1:493675019602:web:667ff39ed68625231c8e8b",
+  measurementId: "G-0TKNPX400Q"
+};
+
+export default firebaseConfig
